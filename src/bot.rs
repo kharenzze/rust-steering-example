c6a4a33372f5b59f0dc146091d0a88a23ac01059
Paolo <str237@gmail.com>
2021-08-02 23:55:01 +0200

    So that bots can be disabled

--- src/bot.rs
+++ src/bot.rs
@@ -6,14 +6,21 @@ use glam::*;
 #[derive(Debug, Default)]
 pub struct Bot {
   pub pos: Vec2,
+  pub disabled: bool,
 }
 
 impl EventHandler<ggez::GameError> for Bot {
   fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+    if self.disabled {
+      return Ok(())
+    }
     Ok(())
   }
   
   fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+    if self.disabled {
+      return Ok(())
+    }
     let mut mb = graphics::MeshBuilder::new();
     mb.circle(DrawMode::fill(), self.pos, 30.0, 1.0, Color::WHITE)?;
     let mesh = mb.build(ctx)?;
